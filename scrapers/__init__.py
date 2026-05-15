00d53033a637025d22e444312709f12780239f97
keikeiiu <keiwarcraft@gmail.com>
2026-05-15 23:16:21 +0800

    cleanup: remove old fc2_db.py, fc2_config.yaml, scrapers/ dir

--- scrapers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""FC2 scraper package. Each site gets its own module."""
-
-from scrapers.base import BaseScraper
-from scrapers.fc2ppvdb_scraper import Fc2ppvdbScraper
-
-SCRAPERS = {
-    "fc2ppvdb": Fc2ppvdbScraper,
-}
